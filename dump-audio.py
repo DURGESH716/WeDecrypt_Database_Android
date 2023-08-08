9dbe009aaf831f0f26747450f399659049c5ec0f
Durgesh Rao <77789927+DURGESH716@users.noreply.github.com>
2023-08-08 14:38:40 +0530

    Add files via upload

---
+++ dump-audio.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import base64
+import argparse
+
+from wechat.parser import WeChatDBParser
+from wechat.msg import TYPE_SPEAK
+from wechat.res import Resource
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('name', help='name of contact')
+    parser.add_argument('--output', help='output mp3 dir', default='/tmp')
+    parser.add_argument('--db', default='decrypted.db', help='path to decrypted database')
+    parser.add_argument('--res', default='resource', help='reseource directory')
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    args = get_args()
+    parser = WeChatDBParser(args.db)
+    res = Resource(parser, args.res, '')
+
+    try:
+        chatid = parser.get_chat_id(args.name)
+    except KeyError:
+        sys.stderr.write(u"Valid Contacts: {}\n".format('\n'.join(parser.all_chat_nicknames)))
+        sys.stderr.write(u"Couldn't find the chat {}.".format(args.name));
+        sys.exit(1)
+
+    msgs = parser.msgs_by_chat[chatid]
+    print(f"Number of Messages for {args.name}: ", len(msgs))
+    assert len(msgs) > 0
+
+    voice_msgs = [m for m in msgs if m.type == TYPE_SPEAK]
+    for idx, m in enumerate(voice_msgs):
+        audio_str, duration = res.get_voice_mp3(m.imgPath)
+        audio_bytes = base64.b64decode(audio_str)
+        outf = f'/{args.output}/{idx:04d}-{duration:.1f}s.mp3'
+        with open(outf, 'wb') as f:
+            f.write(audio_bytes)
+        print(f"Audio written to {outf}")
---
+++ dump-msg.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import logging
+from wechat.parser import WeChatDBParser
+from wechat.common.textutil import safe_filename
+import sys, os
+
+logger = logging.getLogger("wechat")
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        sys.exit("Usage: {0} <path to decrypted_database.db> <output_dir>".format(sys.argv[0]))
+
+    db_file = sys.argv[1]
+    output_dir = sys.argv[2]
+    try:
+        os.mkdir(output_dir)
+    except:
+        pass
+    if not os.path.isdir(output_dir):
+        sys.exit("Error creating directory {}".format(output_dir))
+
+    parser = WeChatDBParser(db_file)
+
+    for chatid, msgs in parser.msgs_by_chat.items():
+        name = parser.contacts[chatid]
+        if len(name) == 0:
+            logger.info(f"Chat {chatid} doesn't have a valid display name.")
+            name = str(id(chatid))
+        logger.info(f"Writing msgs for {name}")
+        safe_name = safe_filename(name)
+        outf = os.path.join(output_dir, safe_name + '.txt')
+        if os.path.isfile(outf):
+            logger.info(f"File {outf} exists! Skip contact {name}")
+            continue
+        with open(outf, 'w') as f:
+            for m in msgs:
+                f.write(str(m))
+                f.write("\n")
